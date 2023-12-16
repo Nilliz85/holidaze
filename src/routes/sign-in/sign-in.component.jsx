dbaf1026dc4684b95f4ff59b522d073d973a28eb
Per Edward Nilsen <nilliz85@gmail.com>
2023-12-16 14:47:41 +0100

    added the sign in component function, added firebase utils + firebase libraries

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup, createUserDocumentAuth } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
+	const logGoogleUser = async () => {
+		const { user } = await signInWithGooglePopup();
+		const userDocRef = await createUserDocumentAuth(user);
+	};
 	return (
 		<div>
 			<h1>Sign In Page</h1>
+			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
 		</div>
 	);
 };
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyD9Gh4hAtuUJB7fI2woV7fRf8UTr-stul4',
+	authDomain: 'royal-rags-db.firebaseapp.com',
+	projectId: 'royal-rags-db',
+	storageBucket: 'royal-rags-db.appspot.com',
+	messagingSenderId: '193685161524',
+	appId: '1:193685161524:web:808f2a2ee87a3c4752ec61',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentAuth = async (userAuth) => {
+	const userDocRef = doc(db, 'users', userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log('error creating the user', error.message);
+		}
+	}
+
+	return userDocRef;
+};
