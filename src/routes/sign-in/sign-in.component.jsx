565f436955773aa5fb66d703c1e8977d6ec0e36b
Per Edward Nilsen <nilliz85@gmail.com>
2023-12-17 14:09:48 +0100

    sign in on redirect instead of popup, work started not complete

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,23 @@
-import { signInWithGooglePopup, createUserDocumentAuth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import { auth, signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentAuth } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
+	useEffect(async () => {
+		const response = await getRedirectResult(auth);
+		console.log(response);
+	}, []);
+
 	const logGoogleUser = async () => {
 		const { user } = await signInWithGooglePopup();
 		const userDocRef = await createUserDocumentAuth(user);
 	};
+
 	return (
 		<div>
 			<h1>Sign In Page</h1>
 			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
+			<button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
 		</div>
 	);
 };
