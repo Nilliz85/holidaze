b99d2670a4134b6066ab68dceebd6869725ad445
Per Edward Nilsen <nilliz85@gmail.com>
2023-12-21 19:19:11 +0100

    finalized auth listener

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,7 @@
-import { useState, useContext } from 'react';
-
+import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
-import { UserContext } from '../../contexts/user.context';
-import { signInWithGooglePopup, createUserDocumentAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+import { signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
 import './sign-in-form.styles.scss';
 
 const defaultFormFields = {
@@ -15,13 +13,10 @@ const SignInForm = () => {
 	const [formFields, setFormFields] = useState(defaultFormFields);
 	const { email, password } = formFields;
 
-	const { setCurrentUser } = useContext(UserContext);
-
 	const resetFormFields = () => setFormFields(defaultFormFields);
 
 	const signInWithGoogle = async () => {
-		const { user } = await signInWithGooglePopup();
-		await createUserDocumentAuth(user);
+		await signInWithGooglePopup();
 	};
 
 	const handleSubmit = async (event) => {
@@ -30,7 +25,6 @@ const SignInForm = () => {
 		try {
 			const { user } = await signInAuthUserWithEmailAndPassword(email, password);
 			resetFormFields();
-			setCurrentUser(user);
 		} catch (error) {
 			switch (error.code) {
 				case 'auth/invalid-credential':
@@ -59,31 +53,13 @@ const SignInForm = () => {
 			<h2>Already have an account?</h2>
 			<span>Sign in with your email and password</span>
 			<form onSubmit={handleSubmit}>
-				<FormInput
-					label='Email'
-					type='email'
-					required
-					onChange={handleChange}
-					name='email'
-					value={email}
-				/>
+				<FormInput label='Email' type='email' required onChange={handleChange} name='email' value={email} />
 
-				<FormInput
-					label='Password'
-					type='password'
-					required
-					onChange={handleChange}
-					name='password'
-					value={password}
-				/>
+				<FormInput label='Password' type='password' required onChange={handleChange} name='password' value={password} />
 
 				<div className='buttons-container'>
 					<Button type='submit'>Sign In</Button>
-					<Button
-						type='button'
-						onClick={signInWithGoogle}
-						buttonType='google'
-					>
+					<Button type='button' onClick={signInWithGoogle} buttonType='google'>
 						Google sign in
 					</Button>
 				</div>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,11 +2,11 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as RoyalLogo } from '../../assets/royal-rags-logo.svg';
 import { UserContext } from '../../contexts/user.context';
+import { signOutAuthUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
-	console.log(currentUser);
 	return (
 		<Fragment>
 			<div className='navigation'>
@@ -17,9 +17,16 @@ const Navigation = () => {
 					<Link className='nav-link' to='/shop'>
 						SHOP
 					</Link>
-					<Link className='nav-link' to='/auth'>
-						SIGN IN
-					</Link>
+					{currentUser ? (
+						<span className='nav-link' onClick={signOutAuthUser}>
+							{' '}
+							SIGN OUT{' '}
+						</span>
+					) : (
+						<Link className='nav-link' to='/auth'>
+							SIGN IN
+						</Link>
+					)}
 				</div>
 			</div>
 			<Outlet />
