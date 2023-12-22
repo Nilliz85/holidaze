677dd8bb9ae825e1507a6610b75a2044dd05b20b
Per Edward Nilsen <nilliz85@gmail.com>
2023-12-22 21:26:30 +0100

    cart icon, cart icon component, shop-data, product cards, products context and shop component

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,13 @@
+import { ReactComponent as ShoppingBagIcon } from '../../assets/shopping-bag.svg';
+import './cart-icon.styles.scss';
+
+const CartIcon = () => {
+	return (
+		<div className='cart-icon-container'>
+			<ShoppingBagIcon className='shopping-bag-icon' />
+			<span className='item-count'>0</span>
+		</div>
+	);
+};
+
+export default CartIcon;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart-dropdown.context';
 import reportWebVitals from './reportWebVitals';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<UserProvider>
+	<BrowserRouter>
+		<UserProvider>
+			<ProductsProvider>
 				<App />
-			</UserProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+			</ProductsProvider>
+		</UserProvider>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
