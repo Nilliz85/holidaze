87aab0c049273710f55040415f508fdd92692885
Per Edward Nilsen <nilliz85@gmail.com>
2023-12-31 14:40:42 +0100

    Enhanced CartItem with quantity adjustment and remove functionality

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -3,20 +3,31 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-item.styles.scss';
 
 const CartItem = ({ cartItem }) => {
-	const { removeItemFromCart } = useContext(CartContext);
+	const { addItemToCart, removeItemFromCart, clearItemFromCart } = useContext(CartContext);
 	const { name, imageUrl, price, quantity } = cartItem;
+
+	const removeItemFromCartHandler = () => removeItemFromCart(cartItem);
+	const addItemFromCartHandler = () => addItemToCart(cartItem);
+	const clearItemFromCartHandler = () => clearItemFromCart(cartItem);
+
 	return (
 		<div className='cart-item-container'>
 			<img src={imageUrl} alt={`${name}`} />
 			<div className='item-details'>
 				<span className='name'>{name}</span>
 				<span className='price'>
-					{quantity} x ${price}
+					<div className='decrement-item-button' onClick={removeItemFromCartHandler}>
+						&#10094;
+					</div>
+					<span className='value'> {quantity} </span>
+					<div className='increment-item-button' onClick={addItemFromCartHandler}>
+						&#10095;
+					</div>
+					<span className='price-value'> x ${price} </span>
 				</span>
 			</div>
-
-			<div className='remove-button' onClick={() => removeItemFromCart(cartItem.id)}>
-				X
+			<div className='clear-item-button' onClick={clearItemFromCartHandler}>
+				&#10005;
 			</div>
 		</div>
 	);
