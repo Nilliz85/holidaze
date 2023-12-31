660aca57c477a46fe0799051774fa16af7085b94
Per Edward Nilsen <nilliz85@gmail.com>
2023-12-31 14:38:53 +0100

    Refractored cartdropdown to include a message saying that the cart is empty, and also made it so the total sum is hidden from the dropdown when cart is empty

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -6,15 +7,23 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
 	const { cartItems } = useContext(CartContext);
+	const navigate = useNavigate();
+
+	const goToCheckoutHandler = () => {
+		navigate('/checkout');
+	};
 
 	return (
 		<div className='cart-dropdown-container'>
 			<div className='cart-items'>
-				{cartItems.map((item) => (
-					<CartItem key={item.id} cartItem={item} />
-				))}
+				{cartItems.length ? (
+					cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+				) : (
+					<span className='empty-cart-message'>Your cart is empty</span>
+				)}
 			</div>
-			<Button>GO TO CHECKOUT</Button>
+			{cartItems.length > 0 && <span className='total'>Total: 0</span>}
+			<Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
 		</div>
 	);
 };
