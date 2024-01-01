4d5cb775bded49f2200192f82ee5fb09b1c3f1ac
Per Edward Nilsen <nilliz85@gmail.com>
2024-01-01 16:34:44 +0100

    Added a category page. shopping page now shows 4 of each category. clicking title takes you to chosen category, saved local data to firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,7 +9,7 @@ import {
 	signOut,
 	onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyD9Gh4hAtuUJB7fI2woV7fRf8UTr-stul4',
@@ -34,6 +34,33 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+	const collectionRef = collection(db, collectionKey);
+	const batch = writeBatch(db);
+
+	objectsToAdd.forEach((object) => {
+		const newDocRef = doc(collectionRef, object.title.toLowerCase());
+		batch.set(newDocRef, object);
+	});
+
+	await batch.commit();
+
+	console.log('collection added');
+};
+
+export const getCategoriesAndDocuments = async (collectionKey) => {
+	const collectionRef = collection(db, 'categories');
+	const q = query(collectionRef);
+
+	const querySnapshot = await getDocs(q);
+	const categoriesMap = querySnapshot.docs.reduce((accumulator, docSnapshot) => {
+		const { title, items } = docSnapshot.data();
+		accumulator[title.toLowerCase()] = items;
+		return accumulator;
+	}, {});
+	return categoriesMap;
+};
 
 export const createUserDocumentAuth = async (userAuth, additionalInformation = {}) => {
 	if (!userAuth) return;
