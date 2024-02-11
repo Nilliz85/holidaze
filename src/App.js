7d15616446a5a2c0526b314e7969d9a17ff9dbb5
Per Edward Nilsen <nilliz85@gmail.com>
2024-02-11 12:20:36 +0100

    app.js used as index as of now, will move later

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import HeroSection from './components/hero-section/hero-section.component';
+import './routes/home/home.styles.scss';
+import Header from './components/header-section/header-section.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const popularVenues = [
+		{
+			id: 1,
+			title: 'Venue 1',
+			imageUrl: 'https://via.placeholder.com/150',
+			linkUrl: 'category1',
+		},
+		{
+			id: 2,
+			title: 'Venue 2',
+			imageUrl: 'https://via.placeholder.com/150',
+			linkUrl: 'category2',
+		},
+		{
+			id: 3,
+			title: 'Venue 3',
+			imageUrl: 'https://via.placeholder.com/150',
+			linkUrl: 'category3',
+		},
+		{
+			id: 4,
+			title: 'Venue 4',
+			imageUrl: 'https://via.placeholder.com/150',
+			linkUrl: 'category4',
+		},
+	];
+
+	return (
+		<div className='landingpage-container'>
+      <Header />
+			<main>
+            <HeroSection />
+				<div className='popular-venues-container'>
+					{popularVenues.map(({ title, id }) => (
+						<div key={id} className='popular-venue-container'>
+							<div className='popular-venue-img' />
+							<div className='popular-venue-body-container'>
+								<h2>{title}</h2>
+								<p>venue information</p>
+							</div>
+						</div>
+					))}
+				</div>
+				<div>info section</div>
+				<div>commercial section</div>
+			</main>
+			<footer>
+				<div>footer section</div>
+			</footer>
+		</div>
+	);
+};
 
 export default App;
