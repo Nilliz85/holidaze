9914490c5fa4b1bb3043026d99b026ba5014b604
Per Edward Nilsen <nilliz85@gmail.com>
2024-02-15 20:42:17 +0100

    Moved index from app.js to home.component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import HeroSection from './components/hero-section/hero-section.component';
-import Header from './components/header-section/header-section.component';
-import PopularVenuesComponent from './components/venues/popular-venues/popular-venues.component';
+import HomeComponent from './routes/home/home.component';
 import './routes/home/home.styles.scss';
 
 const App = () => {
 	return (
-		<div className='landingpage-container'>
-			<Header />
-			<main>
-				<HeroSection />
-				<PopularVenuesComponent />
-				<div>info section</div>
-				<div>commercial section</div>
-			</main>
-			<footer>
-				<div>footer section</div>
-			</footer>
-		</div>
+		<HomeComponent />
 	);
 };
 
